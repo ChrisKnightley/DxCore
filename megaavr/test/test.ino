dcc1811f71d69387f128e686d658499ec1e29b46
ChrisKnightley <80082033+ChrisKnightley@users.noreply.github.com>
2024-05-24 14:42:00 +0900

    Create test.ino

---
+++ megaavr/test/test.ino
@@ -0,0 +1,15 @@
+#if something
+  #if somethingelse
+    doThis();
+    #if defined(SOMEPERIPHERAL)
+      SOMEPERIPHERAL.SFR = foo;
+    #elif defined(SOMEOTHERPERIPHERAL)
+      if (SYSCFG.REVID > 0x08) {
+        SOMEOTHERPERIPHERAL.SFR=getValueForOther(foo);
+      }
+    #else
+      return -1;
+    #endif
+  #endif
+  return DoThatThing();
+#endif
